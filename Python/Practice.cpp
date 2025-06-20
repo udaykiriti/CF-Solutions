52bcc64eb7d27bd40b4cc431487173fab95d83a2
udaykiriti <udaykiriti624@gmail.com>
2025-06-20 09:37:54 +0530

    added

--- Python/Practice.cpp
+++ Python/Practice.cpp
@@ -1,30 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
-#define debug(x) cout << (x) << endl
 
-int sumof(int n)
+#define endl '\n'
+#define FastIO()                    \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(nullptr);                 \
+  cout.tie(nullptr);
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORk(i, a, b, k) for (int i = (a); i <= (b); i += (k))
+#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFORK(i, a, b, k) for (int i = (a); i >= (b); i -= (k))
+
+void solve()
 {
-  int temp = n;
-  int total = 0;
-  while (temp)
-  {
-    total += temp % 10;
-    temp /= 10;
-  }
-  return total;
+  // Your solution logic here
 }
 
 int main()
 {
-  int n;
-  // cin >> n;
-  n = 371;
-  int t = sumof(n);
-  while (t > 10)
-  {
-    t = sumof(t);
-    //debug(t);
-  }
-  cout << t << endl;
+  FastIO();
+  int t = 1;
+  cin >> t;
+  t = 1;
+  while (t--)
+    solve();
+  return 0;
 }

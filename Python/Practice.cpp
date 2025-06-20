f243a88c97a73badccb5d4857efc9a823dd692a8
udaykiriti <udaykiriti624@gmail.com>
2025-06-20 17:01:19 +0530

    added

--- Python/Practice.cpp
+++ Python/Practice.cpp
@@ -1,28 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream> //cin,cout
 using namespace std;
-
-#define endl '\n'
-#define FastIO()                    \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(nullptr);                 \
-  cout.tie(nullptr);
-#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
-#define FORk(i, a, b, k) for (int i = (a); i <= (b); i += (k))
-#define RFOR(i, a, b) for (int i = (a); i >= (b); i--)
-#define RFORK(i, a, b, k) for (int i = (a); i >= (b); i -= (k))
-
-void solve()
-{
-  // Your solution logic here
-}
 
 int main()
 {
-  FastIO();
-  int t = 1;
-  cin >> t;
-  t = 1;
-  while (t--)
-    solve();
-  return 0;
+  int n;
+  cin >> n;
+  cout << n;
 }
--- Python/sample.py
+++
@@ -1,3 +0,0 @@
-n=int(input())
-primes=[77]*(n+1)
-print(primes)

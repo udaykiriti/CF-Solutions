aa9ca5efa11ce34376e05870f4f72697cdf018b4
udaykiriti <udaykiriti624@gmail.com>
2025-06-28 12:30:24 +0530

    Final Template Updated

--- CPP_template.cpp
+++ CPP_template.cpp
@@ -1,103 +1,218 @@
+// Standard Libraries
 #include <bits/stdc++.h>
+#include <chrono>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
-/* clang-format off */
+#define endl '\n'
 
-/* TYPES  */
-#define ll long long
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define vi vector<int>
-#define vll vector<long long>
-#define mii map<int, int>
-#define si set<int>
-#define sc set<char>
-#define vb vector<bool>
+//=======================TIMER========================
+#define START_TIMER auto start_time = chrono::high_resolution_clock::now();
+#define END_TIMER auto end_time = chrono::high_resolution_clock::now(); \
+    cerr << "Time: " << chrono::duration<double, milli>(end_time - start_time).count() << " ms"<<endl;
 
-/* FUNCTIONS */
-#define f(i, s, e) for(long long int i = s; i < e; i++)
-#define cf(i, s, e) for(long long int i = s; i <= e; i++)
-#define rf(i, e, s) for(long long int i = e - 1; i >= s; i--)
+
+// Typedefs
+using ll = long long;
+using i64 = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vs = vector<string>;
+using mii = map<int, int>;
+using si = set<int>;
+using sc = set<char>;
+/*
+// ========== PBDS Shortcuts ==========
+#define ordered_set(T) tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>
+#define ordered_multiset tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update>
+
+#define os_insert(s, val) s.insert(val)
+#define os_erase(s, val) s.erase(val)
+#define os_kth(s, k) *s.find_by_order(k)       // 0-based kth smallest
+#define os_rank(s, val) s.order_of_key(val)    // Count of elements < val
+#define os_size(s) ((int)s.size())
+
+#define oms_insert(ms, val) ms.insert({val, timer++})
+#define oms_erase(ms, val) ms.erase(ms.lower_bound({val, 0}))
+#define oms_kth(ms, k) ms.find_by_order(k)->first
+#define oms_rank(ms, val) ms.order_of_key({val, 0})
+
+// Policy-Based Data Structures
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+
+// Fast I/O
+#define FastIO() ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+
+// Output Utilities
+#define FIXED(x) cout << fixed << setprecision(x)
+#define _flush endl
+#define endl '\n'
+#define debug(x) cout << (x) << endl
+#define debugVec(v) do { cout << #v << " = "; for (auto u : v) cout << u << " "; cout << endl; } while(0)
+#define printm(m) do { cout << "[\n"; for (auto i : m) cout << i.first << " -> " << i.second << endl; cout << "...]\n"; } while(0)
+#define prints(s) do { cout << "{"; for (auto i : s) cout << i << ' '; cout << "}\n"; } while(0)
+
+// Macros & Constants
+#define MOD 1e+7
+#define PI 3.1415926535897932384626433832795
+#define INF 1e18
+#define EPS 1e-9
+#define MAX 1e+7
+#define MIN -1e+7
+
+// STL Shorthand
 #define pb push_back
 #define eb emplace_back
-#define f first
-#define s second
-#define endl '\n'
-#define sp <<" "<<
-#define MOD 1000000007
-#define fora(a) for(auto u:a)
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a * (b / gcd(a, b)))
+#define F first
+#define S second
 #define all(a) (a).begin(), (a).end()
 #define rall(a) (a).rbegin(), (a).rend()
-#define test int tc; cin >> tc; while(tc--)
-#define forn(i, n) for(auto i = 0; i < n; i++)
-#define printv(a) { for(auto u : a) cout << u << " "; cout << endl; }
-#define printm(a) { for(auto u : a) cout << u.f sp u.s << endl; }
-#define op() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define fori(a, b, c) { for(a = c.begin(); a != b; a++) cout << *a << " "; cout << endl; }
-#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed, ios::floatfield);
 
-/* TYPES (redefined) */
-typedef long long ll;
-typedef unsigned long long ull;
-typedef vector<ll> vl;
-typedef vector<int> vi;
-typedef vector<char> vc;
-typedef vector<string> vs;
-typedef vector<int>::iterator vit;
-typedef set<int> si;
-typedef map<int, int> mii;
-const double eps = 1e-9;
+// Looping Macros
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORk(i, a, b, k) for (int i = (a); i < (b); i += k)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); --i)
+#define RFORK(i, a, b, k) for (int i = (a); i >= (b); i -= k)
+#define FORA(a) for (auto u : a)
+#define SCANA(x) for    (auto &i : x) cin >> i
 
-/* PRINTS */
-template <class T>
-void print_v(vector<T>& v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+// Math Utilities
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a) * ((b) / gcd((a), (b))))
+#define sqr(x) ((x) * (x))
+#define cube(x) ((x) * (x) * (x))
+#define isEven(x) ((x) % 2 == 0)
+#define isOdd(x) ((x) % 2 != 0)
+#define uceil(a, b) ((a + b - 1) / (b))
 
-/* UTILS */
-#define PI 3.1415926535897932384626433832795
-#define MOD 1000000007
+#define popcount(x) __builtin_popcountll(x)
+#define ctz(x) __builtin_ctzll(x) // 40 = 101000 → returns 3 (Trailing zeroes)
+#define clz(x) __builtin_clzll(x) // 8 = 1000 → returns 60 (as it's 64-bit) (leading zeroes)
+#define parity(x) __builtin_parityll(x) // 7 = 111 → 3 ones → returns 1 (Parity checks)
 
-ll min(ll a, int b) { if (a < b) return a; return b; }
-ll min(int a, ll b) { if (a < b) return a; return b; }
-ll max(ll a, int b) { if (a > b) return a; return b; }
-ll max(int a, ll b) { if (a > b) return a; return b; }
-ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b); }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-string to_upper(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'a' && a[i] <= 'z') a[i] -= 'a' - 'A'; return a; }
-string to_lower(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'A' && a[i] <= 'Z') a[i] += 'a' - 'A'; return a; }
-bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
-void yes() { cout << "YES\n"; }
-void no() { cout << "NO\n"; }
+// Binary Search Shorthand
+#define bin_sc(a, x) binary_search(all(a), x)
+#define lbd(a, x) lower_bound(all(a), x)
+#define ubd(a, x) upper_bound(all(a), x)
 
-/* All Required define Pre-Processors and typedef Constants */
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int uint64;
-
-/* clang-format on */
-
-/* Main()  function */
-int main()
-{
-#ifndef ONLINE_JUDGE
-  freopen("input.txt", "r", stdin);
-  // freopen("output.txt","w",stdout);
-#endif
-
-  op(); // Optimize input/output
-
-  int tc;
-  cin >> tc; // Input number of test cases
-  while (tc--)
-  {
-    // Your code for each test case here
-    // Example:
-    // int n; cin >> n;
-    // cout << n * n << endl;
-  }
-  return 0;
+// Utility Functions
+ll gcd(ll a, ll b) {
+    return b == 0 ? a : gcd(b, a % b);
 }
 
-/* Main() Ends Here */
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; ++i)
+        if (n % i == 0) return false;
+    return true;
+}
+
+bool isUp(char ch) {
+    locale loc;
+    return isupper(ch, loc);
+}
+
+// Power & Combinatorics
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res *= a;
+        a *= a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll powerMod(ll base, ll exp, ll mod) {
+    return binpow(base, exp, mod);
+}
+
+ll factorialMod(ll n, ll mod) {
+    ll res = 1;
+    for (ll i = 1; i <= n; ++i)
+        res = (res * i) % mod;
+    return res;
+}
+
+i64 NcR(i64 n, i64 r) {
+    i64 x = 1, y = 1;
+    if (n - r < r) r = n - r;
+    while (r) {
+        x *= n;
+        y *= r;
+        i64 cm = gcd(x, y);
+        x /= cm;
+        y /= cm;
+        --n;
+        --r;
+    }
+    return x;
+}
+
+i64 NpR(i64 n, i64 r) {
+    i64 res = 1;
+    while (r--) res *= n--;
+    return res;
+}
+
+// XOR Range Queries
+int XOR1toN(int N) {
+    int md = N % 4;
+    if (md == 0) return N;
+    if (md == 1) return 1;
+    if (md == 2) return N + 1;
+    return 0;
+}
+
+int XORLtoR(int L, int R) {
+    return XOR1toN(R) ^ XOR1toN(max(L - 1, 0));
+}
+
+// Solution Function
+void solve() {
+    // Write your solution logic here
+    int a,b,area; cin >> a >> b;
+    if (b <= a)
+    {
+      if (2 * b < a)
+        area = a * a;
+      else
+        area = 4 * b * b;
+    }
+    else
+    {
+      if (2 * a < b)
+        area = b * b;
+      else
+        area = 4 * a * a;
+    }
+    cout << area << endl;
+
+}
+
+// Main Function
+int main() {
+    FastIO();
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
+}
